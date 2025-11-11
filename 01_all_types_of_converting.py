677db5fc2ea6a8d86e1c8ad9c2b03852e96a6a8d
niksata <niki.kolev2006@gmail.com>
2025-11-11 19:02:27 +0200

    first commit

---
+++ 01_all_types_of_converting.py
@@ -0,0 +1,16 @@
+print('Type of realms: ',
+'\n1-> Length / Distance' , '2-> Temperature','3-> Mass / Weight', '4-> Volume / Capacity',
+'\n5-> Area', '6-> Speed / Velocity', '7-> Time' , '8-> Energy',
+'\n9-> Pressure', '10-> Power' , '11-> Digital Storage / Data',' 12-> Angle')
+
+print('')
+
+realm = input("What type of converting do you want?(Number please): ")
+while not realm.isdigit():
+    realm = input("Enter a number please")
+    if realm.isdigit():
+        continue
+
+if realm == '1':
+    print('length/distance: ')
+    n = int(input("Enter ()"))
